46557af6835f6cbc4111c79b6d36a989c46b4fd4
poo777 <timgansen@yahoo.com>
2020-09-06 16:15:53 -0500

    Add files via upload

---
+++ Lab_5_2Submittal.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab5_2Submittal
+{
+    class Program
+    {
+
+        public enum WeekDays
+        {
+
+            MONDAY,
+            TUESDAY,
+            WEDNESDAY,
+            THURSDAY,
+            FRIDAY,
+            SATURDAY,
+            SUNDAY
+        }
+
+        static void Main(string[] args)
+        {
+            //Create a list tasks
+            List<string> taskList = new List<string>();
+
+            //Day Enum...create a task.
+            foreach (WeekDays day in Enum.GetValues(typeof(WeekDays)))
+            {
+                Console.WriteLine("Enter a new task for " + day);
+                string task = day + " : " + Console.ReadLine();
+                taskList.Add(task);
+
+            }
+
+
+            //print all tasks.
+            foreach (string task in taskList)
+            {
+                Console.WriteLine(task);
+            }
+        }
+    }
+}
